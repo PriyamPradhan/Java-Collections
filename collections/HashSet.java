cae7f13f2be4fb3a789d313cdf872daebb37308f
Priyam Pradhan <priyampradhan07@gmail.com>
2024-07-27 18:48:55 +0530

    added set and hashset

---
+++ collections/HashSet.java
@@ -0,0 +1,33 @@
+package collections;
+
+import java.util.Set;
+
+public class HashSet {
+
+    public static void main(String[] args) {
+        
+        Set<Integer> value = new java.util.HashSet<>();
+
+        value.add(10);
+        value.add(20);
+        value.add(30);
+        value.add(60);
+        value.add(110);
+        value.add(70);
+        value.add(77);
+        value.add(45);
+
+
+        value.remove(20);
+
+        System.out.println("does it contain 10 ? : " + value.contains(10));
+
+
+        System.out.println("is it empty ? : " + value.isEmpty());
+
+
+
+        System.out.println(value);      //arranged randomly
+    }
+
+}
---
+++ collections/LearnSet.java
@@ -0,0 +1,15 @@
+package collections;
+
+public class LearnSet {
+
+    public static void main(String[] args) {
+        
+
+        System.out.println("----------------");
+        System.out.println("----------------");
+        System.out.println("Set only has unique elements");
+        System.out.println("----------------");
+        System.out.println("----------------");
+    }
+
+}
